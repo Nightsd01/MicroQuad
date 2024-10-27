83133ea7ac3ad90f45ab90d12ded5662c34281b4
Brad Hesse <nightsd01@gmail.com>
2024-10-26 19:11:55 -0700

    Add AsyncController
    - The ESP32-S3 is a dual core SoC and executes bluetooth logic on a different core than the primary setUp + loop functions are called on
    - It also has significant issues with ESP32's log functionality and encounters stack overflows whenever I do console logging in bluetooth logic
    - To fix this I am gonna add logger functions like `LOG_INFO_ASYNC_ON_MAIN("...")`. It is not exactly ideal since it could result in non-linear log orderings but it is still better than nothing

---
+++ components/MicroQuad/include/AsyncController.h
@@ -0,0 +1,22 @@
+#ifndef ASYNC_CONTROLLER_H
+#define ASYNC_CONTROLLER_H
+
+#include <functional>
+
+class AsyncController {
+  public:
+    // Static member variables
+    static AsyncController main;
+    static AsyncController background;
+    void execute(std::function<void()> task);
+
+  private:
+    AsyncController(
+      bool core, 
+      const char *taskLabel
+    );
+    bool _core;
+    const char *_taskLabel;
+};
+
+#endif 
---
+++ components/MicroQuad/src/AsyncController.cpp
@@ -0,0 +1,48 @@
+#include "AsyncController.h"
+
+#include "Logger.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define DEFAULT_STACK_SIZE 4096
+#define DEFAULT_TASK_PRIORITY 1
+
+// Define the static member variables
+AsyncController AsyncController::main(1, "main_task");
+AsyncController AsyncController::background(0, "bg_task");
+
+static void _handler(void *pvParams)
+{
+  std::function<void()> *funcPtr = static_cast<std::function<void()> *>(pvParams);
+  (*funcPtr)();
+  delete funcPtr;
+  vTaskDelete(NULL);
+}
+
+AsyncController::AsyncController(
+  bool core, 
+  const char *taskLabel
+) {
+  _core = core;
+  _taskLabel = taskLabel;
+}
+
+void AsyncController::execute(std::function<void()> task)
+{
+  std::function<void()> *funcPtr = new std::function<void()>(std::move(task));
+  BaseType_t result = xTaskCreatePinnedToCore(
+    _handler,               // Task entry function
+    _taskLabel,             // Task name
+    DEFAULT_STACK_SIZE,     // Stack size in bytes
+    funcPtr,                // Parameter passed to the task
+    DEFAULT_TASK_PRIORITY,  // Task priority
+    NULL,                   // Task handle (optional)
+    _core                   // Core ID (0 or 1)
+  );
+
+  if (result != pdPASS) {
+    // Handle task creation failure
+    delete funcPtr; // Clean up allocated memory
+    LOG_ERROR("Failed to create task %s", _taskLabel);
+  }
+}
