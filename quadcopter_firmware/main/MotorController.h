350545ca7404b48b13324bccd61ccd02bfa2ab80
Brad Hesse <nightsd01@gmail.com>
2025-03-14 19:07:09 -0700

    [Firmware] - Remove Motor Telemetry
    - This has been extremely complex to get working and I haven't had time to debug it.
    - BLHeli ESC motor controller firmware allows for 'bi-directional telemetry' where it can, on the same signal GPIO that you use to control it, it can then transmit back a 'telemetry' packet.
    - Even now, when I've created a new PCB design that uses two GPIO's for each ESC signal line (so that I don't need a single GPIO to rapidly switch from being a TX to RX pin, which the ESP32 cannot do quickly enough to capture the telemetry response packet) - I still cannot get this to work properly
    - I will probably spend some time in a few weeks to try to get this to work but for now, no need to keep this in master

--- quadcopter_firmware/main/MotorController.h
+++ quadcopter_firmware/main/MotorController.h
@@ -5,24 +5,13 @@
 
 #include <dshot_esc_encoder.h>
 
-#include "driver/rmt_common.h"
 #include "driver/rmt_rx.h"
 #include "driver/rmt_tx.h"
-
-/** Telemetry data structure for DShot telemetry (voltage, current, etc.) */
-struct TelemetryData
-{
-  float voltage;        // ESC voltage in Volts (0.25 V resolution)
-  float current;        // ESC current in Amps (1 A resolution)
-  uint8_t temperature;  // ESC temperature in °C
-  uint16_t rpm;         // Motor eRPM (electrical RPM) from telemetry
-  uint8_t errorFlags;   // Error/Status flags: bit0=error, bit1=warning, bit2=alert
-};
 
 class MotorController
 {
  public:
-  MotorController(int motorNum, gpio_num_t pin, gpio_num_t telemPin = (gpio_num_t)-1, bool enableTelem = false);
+  MotorController(gpio_num_t pin, gpio_num_t telemPin = (gpio_num_t)-1, bool enableTelem = false);
 
   // Range from MIN_THROTTLE_RANGE to MAX_THROTTLE_RANGE
   void setSpeed(uint16_t speed);
@@ -36,29 +25,14 @@ class MotorController
 
   bool isConnected;
 
-  TelemetryData telemetry;
-
-  // Callback for RMT transmission completion (to handle telemetry)
-  bool _handleRmtTxCompleteCallback(rmt_channel_handle_t channel, const rmt_tx_done_event_data_t *event_data);
-  bool _handleRmtRxCompleteCallback(rmt_channel_handle_t channel, const rmt_rx_done_event_data_t *edata);
-
-  void *_rxBuffer;
+ private:
   gpio_num_t _pin;
   gpio_num_t _telemPin;
   bool _telemEnabled;
   dshot_esc_throttle_t _throttle;
   rmt_channel_handle_t _escChan;
-  rmt_channel_handle_t _escRxChan;
   rmt_encoder_handle_t _encoder;
   rmt_transmit_config_t _txConfig;
-
-  // RMT channel IDs for TX (and RX if using separate channel for telemetry)
-  int _rmtChannelTx;
-  int _rmtChannelRx;  // RMT channel for telemetry receive (if needed)
-
   bool _isHalted = false;
   void _writeRawValue(uint16_t value);
-
-  // Helper to process a raw 16-bit telemetry packet and update telemetry struct
-  void _processTelemetryPacket(uint16_t packet);
 };
